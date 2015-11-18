4be3dc4b14d36cf7ea6b5bd3da74238de3a58220
Do Van Hoang <dovanhoang.it@gmail.com>
2015-11-18 10:25:24 +0700

    demo

--- app/src/main/java/itychange/demo_mutilanguage/MainActivity.java
+++ app/src/main/java/itychange/demo_mutilanguage/MainActivity.java
@@ -1,14 +1,63 @@
 package itychange.demo_mutilanguage;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // String locale = getResources().getConfiguration().locale.getISO3Language();
+        spinner = (Spinner) findViewById(R.id.spinner);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Demo");
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                if (arg2 == 1) {
+                    setLocale("en");
+                } else if (arg2 == 2) {
+                    setLocale("vi");
+                } else if (arg2 == 3) {
+                    setLocale("ja");
+                } else if (arg2 == 4) {
+                    setLocale("zh");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+
+        });
     }
+
+    private void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+    }
+
 }
